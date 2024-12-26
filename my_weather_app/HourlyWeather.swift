361d89098c069505ac0d7eb848650a885077ed91
haidarov_n <nozimjonhaydarov6@gmail.com>
2024-12-26 18:23:42 +0500

    create

---
+++ my_weather_app/HourlyWeather.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  my_weather_app
+//
+//  Created by Haidarov N on 12/26/24.
+//
+
+import Foundation
---
+++ my_weather_app/fetchData.swift
@@ -0,0 +1,8 @@
+//
+//  fetchData.swift
+//  my_weather_app
+//
+//  Created by Haidarov N on 12/25/24.
+//
+
+import Foundation
